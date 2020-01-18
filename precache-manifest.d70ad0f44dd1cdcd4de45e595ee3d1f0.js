e0c8c423668d4ec17da9c6e3d546dc32a202673d
Andrew Vieira <5641660+AndrewVieira@users.noreply.github.com>
2020-01-18 12:08:37 -0600

    Updates

---
+++ precache-manifest.d70ad0f44dd1cdcd4de45e595ee3d1f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a832d78f387138500f55b869f77faf74",
+    "url": "/web-calculator/index.html"
+  },
+  {
+    "revision": "c4f72f2b8a94070d84e3",
+    "url": "/web-calculator/static/css/main.e0c39070.chunk.css"
+  },
+  {
+    "revision": "7ea25829b3a0eb06bdff",
+    "url": "/web-calculator/static/js/2.a7018b6b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/web-calculator/static/js/2.a7018b6b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4f72f2b8a94070d84e3",
+    "url": "/web-calculator/static/js/main.dbf0f50b.chunk.js"
+  },
+  {
+    "revision": "77b129c2e6af6f8ac888",
+    "url": "/web-calculator/static/js/runtime-main.f3373f00.js"
+  }
+]);
