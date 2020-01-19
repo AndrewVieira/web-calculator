faec93c9cc51d915c8462b774a3ba1c2370ab0f0
Andrew Vieira <5641660+AndrewVieira@users.noreply.github.com>
2020-01-19 15:49:06 -0600

    Updates

---
+++ precache-manifest.ab3b0c2b0e3645aff0807a1dcfe4167b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2585bf2f3e4d20d4d6cd9d76846fc21",
+    "url": "/web-calculator/index.html"
+  },
+  {
+    "revision": "f0fc87c97c99327c66d1",
+    "url": "/web-calculator/static/css/main.ad5e9e18.chunk.css"
+  },
+  {
+    "revision": "aeb0183ed505fa6a3968",
+    "url": "/web-calculator/static/js/2.2123e8fa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/web-calculator/static/js/2.2123e8fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f0fc87c97c99327c66d1",
+    "url": "/web-calculator/static/js/main.048e6424.chunk.js"
+  },
+  {
+    "revision": "77b129c2e6af6f8ac888",
+    "url": "/web-calculator/static/js/runtime-main.f3373f00.js"
+  },
+  {
+    "revision": "78ee9c8a6a9f322010a59e932bc83920",
+    "url": "/web-calculator/static/media/vintage-wallpaper.78ee9c8a.png"
+  }
+]);
